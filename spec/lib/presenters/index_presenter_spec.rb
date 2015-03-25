c9b565cb3bdab8f950e401bd905357fe1004e4ea
Grayson Wright <wright.grayson@gmail.com>
2015-03-25 01:18:27 -0700

    Fix issue

--- spec/lib/presenters/index_presenter_spec.rb
+++ spec/lib/presenters/index_presenter_spec.rb
@@ -4,7 +4,6 @@ require "presenters/index_presenter"
 describe IndexPresenter do
   describe "#edit_path" do
     it "returns the edit path for a resource" do
-      raise 'ohno'
       customer = create(:customer)
       presenter = IndexPresenter.new(CustomerDashboard.new)
 
